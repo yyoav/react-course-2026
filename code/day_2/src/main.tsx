8a46727ea1ca5b598c83bb69f6ade18f336febf9
Avraham Hamu 🏂 <ah584d@intl.att.com>
2026-06-03 11:23:26 +0300

    add new exercice

--- code/day_2/src/main.tsx
+++ code/day_2/src/main.tsx
@@ -7,6 +7,7 @@ import KeysExample from "./pages/KeysExample.tsx";
 import ConditionalExample from "./pages/ConditionalExample.tsx";
 import UseEffectClock from "./pages/UseEffectClock.tsx";
 import UseEffectFetch from "./pages/UseEffectFetch.tsx";
+import UseEffectDeps from "./pages/UseEffectDeps.tsx";
 import UseEffectBug from "./pages/UseEffectBug.tsx";
 import UseRefExample from "./pages/UseRefExample.tsx";
 import ReconciliationDemo from "./pages/ReconciliationDemo.tsx";
@@ -20,6 +21,7 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/conditional" element={<ConditionalExample />} />
         <Route path="/useeffect1" element={<UseEffectClock />} />
         <Route path="/useeffect2" element={<UseEffectFetch />} />
+        <Route path="/useeffect3" element={<UseEffectDeps />} />
         <Route path="/bug1" element={<UseEffectBug />} />
         <Route path="/useref" element={<UseRefExample />} />
         <Route path="/reconciliation" element={<ReconciliationDemo />} />
