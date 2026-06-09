d546402a62651bd224af200fb003da21aac7898e
Avraham Hamu 🏂🏼 <gibor7@gmail.com>
2026-06-09 08:36:04 +0300

    fix slides

--- exercises/day-2/task-list/starter/TaskList.tsx
+++ exercises/day-2/task-list/starter/TaskList.tsx
@@ -1,6 +1,7 @@
 export default function TaskList() {
   return (
     <ul className="task-list">
+      {/* Add your code here. */}
       <li>Sample task</li>
     </ul>
   );
--- src/env.d.ts
+++ src/env.d.ts
@@ -5,3 +5,15 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+// Allow importing CSS files
+declare module '*.css' {
+  const content: Record<string, string>
+  export default content
+}
+
+// Allow importing CSS modules
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
